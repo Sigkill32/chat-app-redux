37401b110158f86f390ed4f10ec835ed95c04791
Manoj Bhat <bhatanm@gmail.com>
2019-10-01 13:43:02 +0530

    first commit

--- src/reducer.js
+++ src/reducer.js
@@ -5,10 +5,11 @@ export function reducer(state = initState, action) {
 
   switch (action.type) {
     case "ASYNC_FETCH_DATA":
-      newState.users = action.data.users;
+      newState.users = action.data;
+      console.log("red:", action.data);
       break;
     case "ADD_MESSAGES":
-      newState.users = action.data.users;
+      newState.users = action.data;
       break;
     default:
       break;
